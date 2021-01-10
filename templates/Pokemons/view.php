13d8a57dc0d8bd9c0572e40267be437f91426b57
TheoGicquel <theo.gicquel.work@gmail.com>
2021-01-11 00:54:57 +0100

    remplacement des placeholders du carousel

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -60,13 +60,13 @@
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                              <img class="d-block w-100" src="https://via.placeholder.com/150" alt="First slide">
+                              <img class="d-block w-100" src="<?= h($pokemon->default_front_sprite_url) ?>" alt="First slide">
                             </div>
                             <div class="carousel-item">
-                              <img class="d-block w-100" src="https://via.placeholder.com/150" alt="Second slide">
+                              <img class="d-block w-100" src="<?= h($pokemon->default_back_sprite_url) ?>" alt="Second slide">
                             </div>
                             <div class="carousel-item">
-                              <img class="d-block w-100" src="https://via.placeholder.com/150" alt="Third slide">
+                              <img class="d-block w-100" src="<?= h($pokemon->shiny_front_sprite_url) ?>" alt="Third slide">
                             </div>
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
