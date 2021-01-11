2e5e4b9da1d051733b7523cc94b6770b5f3ac6a9
TheoGicquel <theo.gicquel.work@gmail.com>
2021-01-11 11:17:44 +0100

    ajout carousel non fonctionnel

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -65,10 +65,32 @@
                 <!-- carousel -->
                 <div class="row">
                 <div class="col-sm-12">
-                <img src="<?= h($pokemon->default_front_sprite_url) ?>">
+                    <div>
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <img class="" style="width:20%;height:20%; margin:0 auto;" src="<?= h($pokemon->default_front_sprite_url) ?>" alt="First slide">
+                          </div>
+                          <div class="carousel-item active">
+                            <img class="" style="width:20%;height:20%" src="<?= h($pokemon->default_back_sprite_url) ?>" alt="Second slide">
+                          </div>
+                          <div class="carousel-item active">
+                            <img class="" style="width:20%;height:20%" src="<?= h($pokemon->shiny_front_sprite_url) ?>" alt="Third slide">
+                          </div>
+                        </div>
+                        <a class="carousel-control-prev" style="background-color: grey;" href="#carouselExampleControls" role="button" data-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" style="background-color: grey;" href="#carouselExampleControls" role="button" data-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                <img src="">
                 <img src="<?= h($pokemon->default_back_sprite_url) ?>">
                 <img src="<?= h($pokemon->shiny_front_sprite_url) ?>">
-
+                    </div>
                 </div>
                 </div>
             </div> <!-- /container -->
