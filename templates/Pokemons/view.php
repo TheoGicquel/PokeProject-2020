da3af91f7ee14357417609b3169f186b5e38c8e6
TheoGicquel <theo.gicquel.work@gmail.com>
2021-01-11 02:02:16 +0100

    supression du carousel non conforme

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -65,27 +65,10 @@
                 <!-- carousel -->
                 <div class="row">
                 <div class="col-sm-12">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                              <img class="d-block w-100" src="<?= h($pokemon->default_front_sprite_url) ?>" alt="First slide">
-                            </div>
-                            <div class="carousel-item">
-                              <img class="d-block w-100" src="<?= h($pokemon->default_back_sprite_url) ?>" alt="Second slide">
-                            </div>
-                            <div class="carousel-item">
-                              <img class="d-block w-100" src="<?= h($pokemon->shiny_front_sprite_url) ?>" alt="Third slide">
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                    </div>
+                <img src="<?= h($pokemon->default_front_sprite_url) ?>">
+                <img src="<?= h($pokemon->default_back_sprite_url) ?>">
+                <img src="<?= h($pokemon->shiny_front_sprite_url) ?>">
+
                 </div>
                 </div>
             </div> <!-- /container -->
