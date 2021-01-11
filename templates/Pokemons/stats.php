1dc3de9aa0dce2beaaabffb598bf88d768ac1904
FRANCOIS Valentin <valentin.francois.60@gmail.com>
2021-01-11 10:14:42 +0100

    Add better formatting of code and comments

--- templates/Pokemons/stats.php
+++ templates/Pokemons/stats.php
@@ -3,12 +3,11 @@
 use Cake\ORM\TableRegistry;
 $pokemons = TableRegistry::getTableLocator()->get('pokemons');
 
+
 //Obtention du poid moyen des pokemons de generation ?
 $query = $pokemons->find();
-$query->select(['averageWeight' => $query->func()->avg('weight')])->where(['generation' => 0]);
+$query->select(['averageWeight' => $query->func()->avg('weight')])->where(['generation' => 4]);
 $averageWeight = $query->first();
-
-
 
 
 
@@ -23,8 +22,6 @@ $fairyNumber = $query->first();
 
 
 
-
-
 //Obtention des 10 premier pokemons qui possède la plus grande vitesse
 $query = $pokemons->find()->Join('pokemon_stats')->Join('stats');
 $query->select(['pokemon_stats.value', 'pokemons.name'])
@@ -33,44 +30,51 @@ $query->select(['pokemon_stats.value', 'pokemons.name'])
 ->andwhere(['pokemons.id = pokemon_stats.pokemon_id'])
 ->order(['pokemon_stats.value' => 'DESC'])
 ->limit(10);
-$fastestPokemon = $query->all();
+$fastestPokemons = $query->all();
 
 ?>
-<div class="pokemons index content">
 
-    <h4>Poids moyen des pokémons de la 4éme génération :</h4>
-    <table>
+
+
+
+<!--Mise en forme des information sur la page (stats)-->
+<h4>Poids moyen des pokémons de la 4éme génération :</h4>
+<table>
+    <tr>
+        <th>Poid</th>
+        <td><?= h($averageWeight->averageWeight) ?></td> <!--Affichage de la moyenne du poid des pokemon de generation 4-->
+    </tr>
+</table>
+
+
+
+<h4>Nombre de pokémons de type fée entre les générations 1, 3 et 7 (donc cumule des générations 2, 4, 5 et 6) :</h4>
+<table>
+    <tr>
+        <th>Nombre</th>
+        <td><?= h($fairyNumber->fairyNumber) ?></td> <!--Affichage du nombre de pokemon fée cumulé des generation 2, 4, 5 et 6-->
+    </tr>
+</table>
+
+
+
+<h4>Affichage des 10 premiers pokémons qui possèdent la plus grande vitesse :</h4>
+<table>
+    <tr>
+        <th>Position</th>
+        <th>Nom</td>
+        <th>Vitesse</th>
+    </tr>
+    <?php $index = 1;
+
+    foreach ($fastestPokemons as $pokemon) : ?>
+
         <tr>
-            <th>Poid</th>
-            <td><?= h($averageWeight->averageWeight) ?></td>
+            <th><?= $index ?></th> <!--Affichage de la position dans le classement du pokemon-->
+            <th><?= h($pokemon->name) ?></td> <!--Affichage du nom du pokemon-->
+            <th><?= h($pokemon->pokemon_stats['value']) ?></th> <!--Affichage de la vitesse du pokemon-->
         </tr>
-    </table>
 
-    <h4>Nombre de pokémons de type fée entre les générations 1, 3 et 7 (donc cumule des générations 2, 4, 5 et 6) :</h4>
-    <table>
-        <tr>
-            <th>Nombre</th>
-            <td><?= h($fairyNumber->fairyNumber) ?></td>
-        </tr>
-    </table>
-
-    <table>
-        <tr>
-            <th>Position</th>
-            <th>Nom</td>
-            <th>Vitesse</th>
-        </tr>
-        <?php $index = 1;
-
-        foreach ($fastestPokemon as $pokemon) : ?>
-
-            <tr>
-                <th><?= $index ?></th> <!--Affichage de la position dans le classement du pokemon-->
-                <th><?= h($pokemon->name) ?></td> <!--Affichage du nom du pokemon-->
-                <th><?= h($pokemon->pokemon_stats['value']) ?></th> <!--Affichage de la vitesse du pokemon-->
-            </tr>
-        
-            <?php $index++;
-        endforeach; ?>
-    </table>
-</div>
+        <?php $index++;
+    endforeach; ?>
+</table>
