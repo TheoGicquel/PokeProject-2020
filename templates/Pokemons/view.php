1ceb7e9c1501b09b163340efea896ffc58249805
TheoGicquel <theo.gicquel.work@gmail.com>
2021-01-11 01:27:50 +0100

    supression ancien code original

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -50,9 +50,6 @@
                                     }
                                 ?>
                             </td>
-                            
-                            
-                            
                             <td><?= h($pokemonStats->value) ?></td>
                         </tr>
                         <?php endforeach; ?>
@@ -86,105 +83,6 @@
                     </div>
                 </div>
                 </div>
-<!-- 
-                <div class="row"><!-- page originale --><!-- 
-                    <h3><?= h($pokemon->name) ?></h3>
-                    <table>
-                        <tr>
-                            <th><?= __('Name') ?></th>
-                            <td><?= h($pokemon->name) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Default Front Sprite Url') ?></th>
-                            <td><?= h($pokemon->default_front_sprite_url) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <td><?= $this->Number->format($pokemon->id) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Height') ?></th>
-                            <td><?= $this->Number->format($pokemon->height) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Weight') ?></th>
-                            <td><?= $this->Number->format($pokemon->weight) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Pokedex Number') ?></th>
-                            <td><?= $this->Number->format($pokemon->pokedex_number) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Created') ?></th>
-                            <td><?= h($pokemon->created) ?></td>
-                        </tr>
-                        <tr>
-                            <th><?= __('Modified') ?></th>
-                            <td><?= h($pokemon->modified) ?></td>
-                        </tr>
-                    </table>
-                    <div class="related">
-                        <h4><?= __('Related Pokemon Stats') ?></h4>
-                        <?php if (!empty($pokemon->pokemon_stats)) : ?>
-                        <div class="table-responsive">
-                            <table>
-                                <tr>
-                                    <th><?= __('Id') ?></th>
-                                    <th><?= __('Pokemon Id') ?></th>
-                                    <th><?= __('Stat Id') ?></th>
-                                    <th><?= __('Value') ?></th>
-                                    <th class="actions"><?= __('Actions') ?></th>
-                                </tr>
-                                <?php foreach ($pokemon->pokemon_stats as $pokemonStats) : ?>
-                                <tr>
-                                    <td><?= h($pokemonStats->id) ?></td>
-                                    <td><?= h($pokemonStats->pokemon_id) ?></td>
-                                    <td><?= h($pokemonStats->stat_id) ?></td>
-                                    <td><?= h($pokemonStats->value) ?></td>
-                                    <td class="actions">
-                                        <?= $this->Html->link(__('View'), ['controller' => 'PokemonStats', 'action' => 'view', $pokemonStats->id]) ?>
-                                        <?= $this->Html->link(__('Edit'), ['controller' => 'PokemonStats', 'action' => 'edit', $pokemonStats->id]) ?>
-                                        <?= $this->Form->postLink(__('Delete'), ['controller' => 'PokemonStats', 'action' => 'delete', $pokemonStats->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemonStats->id)]) ?>
-                                    </td>
-                                </tr>
-                                <?php endforeach; ?>
-                            </table>
-                        </div>
-                        <?php endif; ?>
-                    </div>
-                    <div class="related">
-                        <h4><?= __('Related Pokemon Types') ?></h4>
-                        <?php if (!empty($pokemon->pokemon_types)) : ?>
-                        <div class="table-responsive">
-                            <table>
-                                <tr>
-                                    <th><?= __('Id') ?></th>
-                                    <th><?= __('Pokemon Id') ?></th>
-                                    <th><?= __('Type Id') ?></th>
-                                    <th><?= __('Created') ?></th>
-                                    <th><?= __('Modified') ?></th>
-                                    <th class="actions"><?= __('Actions') ?></th>
-                                </tr>
-                                <?php foreach ($pokemon->pokemon_types as $pokemonTypes) : ?>
-                                <tr>
-                                    <td><?= h($pokemonTypes->id) ?></td>
-                                    <td><?= h($pokemonTypes->pokemon_id) ?></td>
-                                    <td><?= h($pokemonTypes->type_id) ?></td>
-                                    <td><?= h($pokemonTypes->created) ?></td>
-                                    <td><?= h($pokemonTypes->modified) ?></td>
-                                    <td class="actions">
-                                        <?= $this->Html->link(__('View'), ['controller' => 'PokemonTypes', 'action' => 'view', $pokemonTypes->id]) ?>
-                                        <?= $this->Html->link(__('Edit'), ['controller' => 'PokemonTypes', 'action' => 'edit', $pokemonTypes->id]) ?>
-                                        <?= $this->Form->postLink(__('Delete'), ['controller' => 'PokemonTypes', 'action' => 'delete', $pokemonTypes->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemonTypes->id)]) ?>
-                                    </td>
-                                </tr>
-                                <?php endforeach; ?>
-                            </table>
-                        </div>
-                        <?php endif; ?>
-                    </div>            
-                </div><!-- Fin page originale -->
-            
             </div> <!-- /container -->
         </div> <!-- pokemons view content -->
     </div><!-- column 80 -->
